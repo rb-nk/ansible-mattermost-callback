e60bf7ba8914b045d055fe9557c32d443090cf62
Ronny Bolzendahl <rbolzendahl@netzkraefte.de>
2022-04-21 20:10:56 +0200

    Update mattermost.py
    added functions and post options
    
    - added runtime for playbook. Message can be customized through timer_format variable
    - added option show_update_result:
        - if set to "True" and update_task_name (value of name tag in playbook with update result infos) is provided, a summery of installed updates is posted
    - added use_bot option: 
        - if you want to us a bot to post in channel instead of a incomming webhook, you can do so by setting this var to "True". Need to provide channel_id aswell!

--- mattermost.py
+++ mattermost.py
@@ -63,6 +63,47 @@ DOCUMENTATION = '''
             key: validate_certs
         default: True
         type: bool
+      timer_format:
+        desciption: format of the string shown to user at play end
+        ini:
+          - section: callback_mattermost
+            key: timer_format
+        env:
+          - name: MATTERMOST_TIMER_FORMAT
+        default: "Playbook run took %s days, %s hours, %s minutes, %s seconds"
+      show_update_result:
+        description: show hostwise installed updates
+        env:
+          - name: MATTERMOST_SHOW_UPDATE_RESULT
+        ini:
+          - section: callback_mattermost
+            key: show_update_result
+        default: False
+        type: bool
+      update_task_name:
+        description: name of task, that returns update infos
+        env:
+          - name: MATTERMOST_UPDATE_TASK_NAME
+        ini:
+          - section: callback_mattermost
+            key: update_task_name
+      use_bot:
+        descrition: use bot instead of webhook. NOTE Need channel_id to be set if bot should be used
+        env:
+          - name: MATTERMOST_USE_BOT
+        ini:
+          - section: callback_mattermost
+            key: use_bot
+        default: false
+        type: bool
+      channel_id:
+        description: id of the channel to post in
+        env:
+          - name: MATTERMOST_CHANNEL_ID
+        ini:
+         - section: callback_mattermost
+           key: channel_id
+        
 '''
 
 import json
@@ -80,6 +121,7 @@ try:
 except ImportError:
     HAS_PRETTYTABLE = False
 
+from datetime import datetime
 
 class CallbackModule(CallbackBase):
     """This is an ansible callback plugin that sends status
@@ -101,6 +143,10 @@ class CallbackModule(CallbackBase):
                                   'plugin.')
 
         self.playbook_name = None
+        self.updateRes = {}
+
+        #start timer when plugin is loaded
+        self.start_time = datetime.now()
 
         # This is a 6 character identifier provided with each message
         # This makes it easier to correlate messages when there are more
@@ -110,12 +156,16 @@ class CallbackModule(CallbackBase):
     def set_options(self, task_keys=None, var_options=None, direct=None):
 
         super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
-
+        self.use_bot = self.get_option('use_bot')
         self.url = self.get_option('mattermost_url')
         self.api_key = self.get_option('api_key')
-        self.webhook_url = "{0}/hooks/{1}".format(self.url, self.api_key)
+        if self.use_bot:
+            self.webhook_url = "{0}/api/v4/posts".format(self.url)
+        else:
+            self.webhook_url = "{0}/hooks/{1}".format(self.url, self.api_key)
         #self.webhook_url = self.get_option('webhook_url')
         self.channel = self.get_option('channel')
+        self.channel_id = self.get_option('channel_id')
         self.username = self.get_option('username')
         self.show_invocation = (self._display.verbosity > 1)
         self.validate_certs = self.get_option('validate_certs')
@@ -136,11 +186,24 @@ class CallbackModule(CallbackBase):
 
         payload = {
             'channel': self.channel,
+            'channel_id': self.channel_id,
             'username': self.username,
             'attachments': attachments,
             'text': text,
             'icon_url': self.icon_url,
         }
+
+        if self.use_bot:
+            headers = {
+              'Content-type': 'application/json',
+              'Authorization': 'Bearer ' + self.api_key,
+            }
+            payload = {
+                'channel_id': self.channel_id,
+                'props': {
+                    'attachments': attachments,
+                }
+            }
 
         data = json.dumps(payload)
         self._display.debug(data)
@@ -150,8 +213,14 @@ class CallbackModule(CallbackBase):
                                 headers=headers)
             return response.read()
         except Exception as e:
-            self._display.warning(u'Could not submit message to Mattermost: %s' %
-                                  to_text(e))
+            self._display.warning('Could not submit message to Mattermost: %s' %
+                                  to_text(e) + data + ' :: ' + self.webhook_url)
+
+    def _days_hours_minutes_seconds(self, runtime):
+        #helper for time format
+        minutes = (runtime.seconds // 60) % 60
+        r_seconds = runtime.seconds - (minutes * 60)
+        return runtime.days, runtime.seconds // 3600, minutes, r_seconds
 
     def v2_playbook_on_start(self, playbook):
         self.playbook_name = os.path.basename(playbook._file_name)
@@ -205,7 +274,6 @@ class CallbackModule(CallbackBase):
     def v2_playbook_on_play_start(self, play):
         """Display Play start messages"""
         
-        text = ''
         name = play.name or 'Play name not specified (%s)' % play._uuid
         msg = '*Starting play* (_%s_)\n\n*%s*' % (self.guid, name)
         attachments = [
@@ -216,16 +284,57 @@ class CallbackModule(CallbackBase):
                 'mrkdwn_in': ['text', 'fallback', 'fields'],
             }
         ]
-        self.send_msg(attachments=attachments, text=text)
+        self.send_msg(attachments=attachments)
+
+    def v2_runner_on_ok(self, result):
+        """gather info about result output, save them for stats use"""
+        host = result._host.get_name()
+        
+        text = host + ' :: ' + self._dump_results(result._result) + ' || ' + result._task.get_name()
+
+        #for key in list(result.keys()):
+        #    text += key + ' || ' + self._dump_results(result[key],indent=4) + "\n"
+        
+
+        attachments = [
+            {
+                'fallback': text,
+                'text': text,
+                'color': 'good',
+                'mrkdwn_in': ['text', 'fallback', 'fields'],
+            }
+        ]
+
+        if self.get_option('show_update_result') == True and result._task.get_name() == self.get_option('update_task_name'):
+            self.updateRes[host] = result._result
+
+
+        #self.updateRes[host] = result._result
+
+        # for k in result._result.updates:
+        #   self.updateRes[host].append({
+        #       'update': result._result.updates[k].title + ' - ' + 'installed: ' + result._result.updates[k].installed
+        #   })
+         
+        # self.send_msg(attachments=attachments)
+        
 
     def v2_playbook_on_stats(self, stats):
         """Display info about playbook statistics"""
 
         text = ''
+
+        end_time = datetime.now()
+        runtime = end_time - self.start_time
+
+        text = self._plugin_options['timer_format'] % (self._days_hours_minutes_seconds(runtime))
+
         hosts = sorted(stats.processed.keys())
 
         t = prettytable.PrettyTable(['Host', 'Ok', 'Changed', 'Unreachable',
                                      'Failures', 'Rescued', 'Ignored'])
+
+        resT = prettytable.PrettyTable(['Host', 'Failed updates', 'Installed Updates', 'Reboot Required', "Updates"])
 
         failures = False
         unreachable = False
@@ -240,6 +349,13 @@ class CallbackModule(CallbackBase):
 
             t.add_row([h] + [s[k] for k in ['ok', 'changed', 'unreachable',
                                             'failures', 'rescued', 'ignored']])
+            if h in self.updateRes.keys():
+                resText = json.dumps(self.updateRes[h])
+                updates = ''
+                for u in self.updateRes[h]['updates']:
+                    updates = self.updateRes[h]['updates'][u].title + ' :: installed: ' + self.updateRes[h]['updates'][u].installed + "\n"
+
+                resT.add_row([h] + [self.updateRes[h][v] for v in ['failed_update_count', 'installed_update_count', 'reboot_required']] + [updates])
 
         attachments = []
         msg_items = [
@@ -252,6 +368,7 @@ class CallbackModule(CallbackBase):
             color = 'good'
             msg_items.append('\n*Success!*')
 
+        msg_items.append('```\n%s\n```' % resT)
         msg_items.append('```\n%s\n```' % t)
 
         msg = '\n'.join(msg_items)
